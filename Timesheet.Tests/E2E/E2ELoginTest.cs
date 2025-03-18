a73d9290206561d6ba06fbd1d50ae0cc818029c3
Yuliya Daniliuk <yuliya.daniliuk@gmail.com>
2025-03-18 17:46:02 +0000

    added e2e user can auth test

--- Timesheet.Tests/E2E/E2ELoginTest.cs
+++ Timesheet.Tests/E2E/E2ELoginTest.cs
@@ -54,6 +54,41 @@ public class E2ELoginTest
    [Test]
    public void StanCanAuth() 
    {
+    DataBuilder dataBuilder = new DataBuilder();
+    TimesheetCredential credentials = dataBuilder.GetUserCredentials("user");
+
+       // Set up the WebDriver for Chrome using WebDriverManager.
+       new DriverManager().SetUpDriver(new ChromeConfig());
+
+       // Initialize a new instance of the ChromeDriver.
+       IWebDriver _webDriver = new ChromeDriver();
+
+       var devTools = _webDriver as IDevTools;
+       var session = devTools.GetDevToolsSession();
+       var domains = session.GetVersionSpecificDomains<DevToolsSessionDomains>();
+       domains.Network.Enable(new Network.EnableCommandSettings());
+       Network.EmulateNetworkConditionsCommandSettings command = new Network.EmulateNetworkConditionsCommandSettings
+        {
+            Latency = 1000,
+        };
+        domains.Network.EmulateNetworkConditions(command);
+
+       // Open a web page with the given URL in the Chrome browser.
+       _webDriver.Navigate().GoToUrl("http://localhost:8080");
+       
+       // Create an instance of the LoginPage class, which is a custom class.
+       LoginPage loginPage = new LoginPage(_webDriver);
+
+       // Perform actions on the login page: sending email, password, and submitting the form.
+       loginPage.SendEmail(credentials.Email);
+       loginPage.SendPassword(credentials.Password);
+       loginPage.SubmitForm();
+
+        ProjectsPage projectsPage = new ProjectsPage(_webDriver);
+        Assert.IsTrue(projectsPage.GetTitle() == "Projects");
+
+        _webDriver.Close();
+        _webDriver.Quit();
 
 
    }
